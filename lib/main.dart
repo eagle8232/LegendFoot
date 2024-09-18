1377557bc6e4dadd2bf8e02c87ae38a28520537b
eagle8232 <zevs.1418@gmail.com>
2024-09-18 07:30:18 +0400

    IGNORE

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_view.dart';
 import 'transferred_players_view.dart';
-import 'create_match_view.dart'; // Import the Create Match View
+import 'create_match_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,15 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'LegendFoot',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white, // Set the scaffold background color to white
+        scaffoldBackgroundColor: Colors.white,
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.deepPurple, // Customize AppBar background color
+          backgroundColor: Colors.deepPurple,
           titleTextStyle: TextStyle(
-            color: Colors.white, // Set title text color to white
-            fontSize: 20, // You can customize the font size as well
-            fontWeight: FontWeight.bold, // Customize the font weight
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          iconTheme: IconThemeData(color: Colors.white), // Set icon color to white
+          iconTheme: IconThemeData(color: Colors.white),
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -39,7 +39,7 @@ class MainTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // Now there are three tabs
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('LegendFoot'),
@@ -47,20 +47,20 @@ class MainTabBar extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            HomeView(), // Home View tab
-            TransferredPlayersView(), // Transferred Players View tab
-            CreateMatchView(), // Create Match View tab
+            HomeView(),
+            TransferredPlayersView(),
+            CreateMatchView(),
           ],
         ),
         bottomNavigationBar: const TabBar(
           tabs: [
             Tab(text: 'Home', icon: Icon(Icons.home)),
             Tab(text: 'Transfers', icon: Icon(Icons.swap_horiz)),
-            Tab(text: 'Create Match', icon: Icon(Icons.add_circle_outline)), // New tab for creating matches
+            Tab(text: 'Create Match', icon: Icon(Icons.add_circle_outline))
           ],
           labelColor: Colors.white,
           unselectedLabelColor: Colors.white70,
-          indicator: BoxDecoration(), // Remove the default indicator
+          indicator: BoxDecoration(),
         ),
         backgroundColor: const Color(0xFF0A0F2A),
       ),
